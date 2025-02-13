045fb3bbc7e208d8f997a7922000310ee28a9666
Ritin George <ritingeorge.d@gmail.com>
2025-02-13 18:07:07 +0530

    feat(ui): enhance VersionBadge styling and text presentation

--- src/components/ui/VersionBadge.tsx
+++ src/components/ui/VersionBadge.tsx
@@ -1,7 +1,7 @@
 export const VersionBadge = () => {
     return (
-      <div className="text-white opacity-70 border border-white w-fit px-2 py-1 rounded-full text-xs my-5">
-        <span className="blur-[0.4px]">version 3.0</span>
+      <div className="text-white opacity-70 border leading-none border-white w-fit px-2 pt-1 pb-[2px] font-light tracking-wider rounded-full text-xs my-5 drop-shadow-[0px_0px_3px_rgba(255,255,255,1)]">
+        Version 3.0
       </div>
     );
   };
